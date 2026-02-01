34ff79c7e299b117519d21f60fc64ef911985376
ADITH6452003 <adith6452@gmail.com>
2026-02-01 10:08:40 +0530

    adding the changes

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+print("XGBoost accuraacy and prediction\n")
+
 import joblib
 import pandas as pd
 import pandas as pd
 df=pd.read_csv("/home/adith6452/Documents/cropprediction/data/Crop_Data.xlsx.csv")
 
 # Load model
-rf = joblib.load("rf_crop_model.pkl")
+xgb = joblib.load("xgb_crop_model.pkl")
 crop_map = joblib.load("crop_label_map.pkl")
 
-# Sample input
 sample = pd.DataFrame(
-    [[26, 80, 6.5, 220]],
+    [[29, 75, 6.8, 180]],
     columns=['temperature', 'humidity', 'ph', 'rainfall']
 )
-
-proba = rf.predict_proba(sample)[0]
+proba = xgb.predict_proba(sample)[0]
 
 # Create mapping
 crop_map = dict(zip(df['Label_Num'], df['label']))
@@ -23,13 +23,10 @@ proba_df = pd.DataFrame({
     'Crop': [crop_map[i] for i in range(len(proba))],
     'Suitability_Probability': proba
 })
-
 # Sort by probability
 proba_df = proba_df.sort_values(by='Suitability_Probability', ascending=False)
-
 # Select Top 5 crops
-top_crops = proba_df.head(5)
-
+top_crops = proba_df.head(3)
 print(top_crops)
 from sklearn.model_selection import train_test_split
 x = df[['temperature', 'humidity', 'ph', 'rainfall']]
@@ -38,6 +35,6 @@ y = df['Label_Num']
 x_train,x_text,y_train,y_test=train_test_split(x,y,random_state=42,test_size=0.2,stratify=y)
 
 from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(y_test, rf.predict(x_text))
+accuracy = accuracy_score(y_test, xgb.predict(x_text))
 print("accuracy = ",accuracy)
 print("Prediction completed without retraining.")
--- model.py
+++ model.py
@@ -7,7 +7,7 @@ df=pd.read_csv("/home/adith6452/Documents/cropprediction/data/Crop_Data.xlsx.csv
 # print(df.describe())
 # Features and target
 x = df[['temperature', 'humidity', 'ph', 'rainfall']]
-y = df['Label_Num']
+y = df['Label_Num'] 
 
 #import training and testing data
 
@@ -18,34 +18,42 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=42,test_size=0.2
 
 #random forest model training
 
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
-rf = RandomForestClassifier(
-    n_estimators=200,
-    max_depth=None,
-    random_state=42,
-    n_jobs=-1
+from xgboost import XGBClassifier
+
+xgb = XGBClassifier(
+    objective="multi:softprob",   # probability output
+    num_class=22,
+    n_estimators=300,
+    max_depth=6,
+    learning_rate=0.05,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    eval_metric="mlogloss",
+    random_state=42
 )
 
-rf.fit(x_train, y_train)
+xgb.fit(x_train, y_train)
+
 #to convert thee output in numbers to the feature values
 crop_map = dict(zip(df['Label_Num'], df['label']))
 #saving the model
 import joblib
 
-joblib.dump(rf, "rf_crop_model.pkl")
+joblib.dump(xgb, "xgb_crop_model.pkl")
 joblib.dump(crop_map, "crop_label_map.pkl")
 
 print("Model and label map saved successfully.")
 
 
-y_pred = rf.predict(x_test)
+y_pred = xgb.predict(x_test)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test,y_pred)
+print(accuracy)
 #evaluating the model
 
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
 
-accuracy = accuracy_score(y_test,y_pred)
-print("accuracy = ",accuracy)
 
 # print("clssification report\n")
 # print(classification_report(y_test,y_pred))
